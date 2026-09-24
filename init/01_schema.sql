80629161082b9eafd5671f023507c26782b628f3
Potapchik Anastasia <apotapcik2l@gmail.com>
2026-09-24 17:20:26 +0300

    З01: новые данные

--- init/01_schema.sql
+++ init/01_schema.sql
@@ -1,47 +1,42 @@
--- Учебная БД курса OLAP (ClickHouse)
 CREATE DATABASE IF NOT EXISTS retail_dw;
-
-CREATE TABLE IF NOT EXISTS retail_dw.dim_store
-(
-    store_id UInt32,
-    store_name String,
-    city String,
-    region String
-)
-ENGINE = MergeTree
-ORDER BY store_id;
 
 CREATE TABLE IF NOT EXISTS retail_dw.dim_product
 (
     product_id UInt32,
     product_name String,
     category String,
-    brand String
+    brand String,
+    unit String,
+    price Float64,
+    supplier_id UInt32
 )
 ENGINE = MergeTree
 ORDER BY product_id;
 
-CREATE TABLE IF NOT EXISTS retail_dw.dim_date
+CREATE TABLE IF NOT EXISTS retail_dw.dim_customer
 (
-    date_id UInt32,
-    full_date Date,
-    year UInt16,
-    month UInt8,
-    month_name String,
-    day_of_week UInt8
+    customer_id UInt32,
+    full_name String,
+    gender String,
+    age UInt8,
+    city String,
+    loyalty_card String,
+    register_date Date
 )
 ENGINE = MergeTree
-ORDER BY date_id;
+ORDER BY customer_id;
 
--- Партиции по месяцу добавим на P07–P08; на старте — простой MergeTree
 CREATE TABLE IF NOT EXISTS retail_dw.fact_sales
 (
     sale_id UInt64,
-    date_id UInt32,
-    store_id UInt32,
+    sale_datetime DateTime,
+    customer_id UInt32,
     product_id UInt32,
-    qty Int32,
-    amount Decimal(12, 2)
+    quantity UInt8,
+    unit_price Float64,
+    total_amount Float64,
+    payment_type String,
+    store_id UInt8
 )
 ENGINE = MergeTree
-ORDER BY (date_id, store_id, product_id);
+ORDER BY (sale_datetime, store_id, product_id);
